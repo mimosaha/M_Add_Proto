4ce153f95c62bd696fbfdd53648ce5c8d458dda0
Mimo <mimosaha@gmail.com>
2019-01-10 13:05:38 +0600

    Init commit with travis

---
+++ app/src/main/java/portal/remote/superpeer/remote/com/m_add_proto/MainActivity.java
@@ -0,0 +1,13 @@
+package portal.remote.superpeer.remote.com.m_add_proto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
